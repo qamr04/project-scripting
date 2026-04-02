3570bea11d101f217a0bcc092182b5992be4e62f
ayman <aymanbouhallil1996@gmail.com>
2026-04-02 14:29:53 +0200

    menu afgemaakt met switch keuze en default

--- menu.ps1
+++ menu.ps1
@@ -1,14 +1,24 @@
-clear-host
 do{
+    clear-host
     write-host "Welkom dit is mijn project voor scripting"
-    [int] $keuze = read-host "Dit is de menu kies hier uit
+    write-host "Dit is de menu kies hier uit
     1) Basisconfiguratie
     2) Domain configuratie
-    3) Stoppen
-    Wat is je keuze"
+    3) Stoppen"
+    [int] $keuze = read-host "Wat is je keuze"
+    switch ($keuze) {    
+            1 { Write-Host "dingus"}
+            2 { Write-Host "binusds"}
+            3 { 
+                Clear-Host
+                write-host "Programma stopt" 
+            }
+        Default {
+            Write-Host "Ongeldige keuze, kies 1,2 of 3"
+            Read-Host "Geef een keuze in"
+        }
+    }
 }
 
-while ($keuze -ne 3){
+while ($keuze -ne 3)
 
-write-host "good jobb"
-}
