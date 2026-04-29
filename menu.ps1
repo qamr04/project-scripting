262cbc9059470fc7454588cfb118553c4d0fae19
qamr04 <aymanbouhallil1996@gmail.com>
2026-04-29 12:01:00 +0200

    Update menu options and improve user prompts

--- menu.ps1
+++ menu.ps1
@@ -1,24 +1,42 @@
-do{
-    clear-host
-    write-host "Welkom dit is mijn project voor scripting"
-    write-host "Dit is de menu kies hier uit
-    1) Basisconfiguratie
-    2) Domain configuratie
-    3) Stoppen"
-    [int] $keuze = read-host "Wat is je keuze"
-    switch ($keuze) {    
-            1 { Write-Host "dingus"}
-            2 { Write-Host "binusds"}
-            3 { 
-                Clear-Host
-                write-host "Programma stopt" 
-            }
+Import-Module "C:\Program Files\WindowsPowerShell\Modules\project-scripting\algemeenBOAY.psm1"
+
+do {
+    Clear-Host
+    Write-Host "Welkom dit is mijn project voor scripting"
+    Write-Host ""
+    Write-Host "1) Basisconfiguratie server"
+    Write-Host "2) Basisconfiguratie workstation"
+    Write-Host "3) Domain configuratie"
+    Write-Host "4) Stoppen"
+    Write-Host ""
+
+    [int]$keuze = Read-Host "Wat is je keuze"
+
+    switch ($keuze) {
+        1 {
+            Get-Servernaam
+            Read-Host "Druk op Enter om verder te gaan"
+        }
+
+        2 {
+            Get-Workstationnaam
+            Read-Host "Druk op Enter om verder te gaan"
+        }
+
+        3 {
+            Write-Host "Domain configuratie"
+            Read-Host "Druk op Enter om verder te gaan"
+        }
+
+        4 {
+            Clear-Host
+            Write-Host "Programma stopt"
+        }
+
         Default {
-            Write-Host "Ongeldige keuze, kies 1,2 of 3"
-            Read-Host "Geef een keuze in"
+            Write-Host "Ongeldige keuze, kies 1, 2, 3 of 4"
+            Read-Host "Druk op Enter om verder te gaan"
         }
     }
-}
 
-while ($keuze -ne 3)
-
+} while ($keuze -ne 4)
